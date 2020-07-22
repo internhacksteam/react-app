2fc084acf29a5db5f4e53d2b134b5f228eac0a24
Niveda <niveda4@hotmail.com>
2020-07-22 12:37:53 -0400

    Create Map.js

---
+++ src/views/examples/Map.js
@@ -0,0 +1,89 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classnames from "classnames";
+import { Link } from "react-router-dom";
+
+// reactstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardImg,
+  FormGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+} from "reactstrap";
+
+// core components
+import DemoNavbar from "components/Navbars/DemoNavbar.js";
+import LevelDisplay from "../IndexSections/LevelDisplay.js";
+
+
+class Map extends React.Component {
+  state = {};
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <DemoNavbar />
+        <main ref="main">
+          <div className="position-relative">
+            {/* shape Hero */}
+            <section className="section section-shaped section-lg">
+              <div className="shape shape-style-1 bg-gradient-default">
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+              </div>
+              <Container>
+                <Link to="/levels-page" tag={Link}>
+                <CardImg src={require("assets/img/theme/map.png")}
+                />
+                </Link>
+              </Container>
+              {/* SVG separator */}
+              <div className="separator separator-bottom">
+                
+              </div>
+            </section>
+
+          </div>
+        </main>
+      
+      </>
+    );
+  }
+}
+
+export default Map;
