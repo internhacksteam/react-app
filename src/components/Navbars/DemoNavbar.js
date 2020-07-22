4850cc84985b25e9601548546c4ec5a7512ab764
Isabel Alvarado Blanco Uribe <alvaradoblancouribe@gmail.com>
2020-07-22 12:00:58 -0400

    the basic log in with Auth0

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -39,98 +39,93 @@ import {
   UncontrolledTooltip
 } from "reactstrap";
 
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../logout-button";
+import LoginButton from "../login-button";
+
 class DemoNavbar extends React.Component {
-  componentDidMount() {
-    let headroom = new Headroom(document.getElementById("navbar-main"));
-    // initialise
-    headroom.init();
-  }
-  state = {
-    collapseClasses: "",
-    collapseOpen: false
-  };
 
-  onExiting = () => {
-    this.setState({
-      collapseClasses: "collapsing-out"
-    });
-  };
+    componentDidMount() {
+      let headroom = new Headroom(document.getElementById("navbar-main"));
+      // initialise
+      headroom.init();
+    }
 
-  onExited = () => {
-    this.setState({
-      collapseClasses: ""
-    });
-  };
+    state = {
+      collapseClasses: "",
+      collapseOpen: false
+    };
 
-  render() {
-    return (
-      <>
+    onExiting = () => {
+      this.setState({
+        collapseClasses: "collapsing-out"
+      });
+    };
+
+    onExited = () => {
+      this.setState({
+        collapseClasses: ""
+      });
+    };
+
+    render() {
+      return (
+        <>
         <header className="header-global">
-          <Navbar
-            className="navbar-main navbar-transparent navbar-light headroom"
-            expand="lg"
-            id="navbar-main"
-          >
-            <Container>
-              <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
-                <img
-                  alt="..."
-                  src={require("assets/img/brand/CHlogo.png")}
-                />
-              </NavbarBrand>
-              <button className="navbar-toggler" id="navbar_global">
-                <span className="navbar-toggler-icon" />
-              </button>
-              <UncontrolledCollapse
-                toggler="#navbar_global"
-                navbar
-                className={this.state.collapseClasses}
-                onExiting={this.onExiting}
-                onExited={this.onExited}
-              >
-                <div className="navbar-collapse-header">
-                  <Row>
-                    <Col className="collapse-brand" xs="6">
-                      <Link to="/">
-                        <img
-                          alt="..."
-                          src={require("assets/img/brand/argon-react.png")}
-                        />
-                      </Link>
-                    </Col>
-                    <Col className="collapse-close" xs="6">
-                      <button className="navbar-toggler" id="navbar_global">
-                        <span />
-                        <span />
-                      </button>
-                    </Col>
-                  </Row>
-                </div>
-                <Nav className="align-items-lg-center ml-lg-auto" navbar>
-                
-                  <NavItem className="d-none d-lg-block ml-lg-4">
-                    <Button to="/login-page" tag={Link}
-                      className="btn-neutral btn-icon"
-                      color="default"
-                      href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-navbar"
-                      target="_blank"
-                    >
-                      <span className="btn-inner--icon">
-                        <i className="fa fa-arrow-circle-right" />
-                      </span>
-                      <span className="nav-link-inner--text ml-1">
-                        Log In
-                      </span>
-                    </Button>
-                  </NavItem>
-                </Nav>
-              </UncontrolledCollapse>
-            </Container>
-          </Navbar>
-        </header>
-      </>
-    );
-  }
-}
+        <Navbar
+        className="navbar-main navbar-transparent navbar-light headroom"
+        expand="lg"
+        id="navbar-main"
+        >
+        <Container>
+        <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
+        <img
+        alt="..."
+        src={require("assets/img/brand/CHlogo.png")}
+        />
+        </NavbarBrand>
+        <button className="navbar-toggler" id="navbar_global">
+        <span className="navbar-toggler-icon" />
+        </button>
 
-export default DemoNavbar;
+        <UncontrolledCollapse
+        toggler="#navbar_global"
+        navbar
+        className={this.state.collapseClasses}
+        onExiting={this.onExiting}
+        onExited={this.onExited}
+        >
+        <div className="navbar-collapse-header">
+        <Row>
+        <Col className="collapse-brand" xs="6">
+        <Link to="/">
+        <img
+        alt="..."
+        src={require("assets/img/brand/argon-react.png")}
+        />
+        </Link>
+        </Col>
+        <Col className="collapse-close" xs="6">
+        <button className="navbar-toggler" id="navbar_global">
+        <span />
+        <span />
+        </button>
+        </Col>
+        </Row>
+        </div>
+        <Nav className="align-items-lg-center ml-lg-auto" navbar>
+
+        <NavItem className="d-none d-lg-block ml-lg-4">
+        <LoginButton/>
+        </NavItem>
+        </Nav>
+        </UncontrolledCollapse>
+        </Container>
+        </Navbar>
+        </header>
+        </>
+        );
+    }
+  }
+
+  export default DemoNavbar;
